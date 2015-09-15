7d59aae4419ffb0068a888b0be0c165c24f07b72
wuweiming <1063502184@qq.com>
2015-09-15 21:48:08 +0800

    第一次提交

---
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
