416e98d3304e64b778b0b4cde38ff4d9cc3d3677
wuweiming <1063502184@qq.com>
2015-09-15 22:35:05 +0800

    添加j

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    public int j ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
